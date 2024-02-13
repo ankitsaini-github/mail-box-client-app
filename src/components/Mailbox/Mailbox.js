39565281b1970fc9f9527d8a6ffd1ae163cc3130
ankitsaini-github <ankit121saini@gmail.com>
2024-02-13 06:06:07 +0530

    text editor added to mailbox

--- src/components/Mailbox/Mailbox.js
+++ src/components/Mailbox/Mailbox.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from 'draft-js';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { Button, Form} from 'react-bootstrap';
 
 const Mailbox = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+  const sendmail=() => {
+    console.log(convertToRaw(editorState.getCurrentContent()))
+  }
   return (
-    <div>Welcome to your Mail Box</div>
+    <div>
+      <Form className='mx-3 p-3 border mt-4'>
+        <Form.Group className="mb-3 text-start" controlId="toemail">
+          <Form.Label>To :</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+
+        <Form.Group className="mb-4 text-start" controlId="emailsubject">
+          <Form.Label>Subject :</Form.Label>
+          <Form.Control type="text" placeholder="Subject" />
+        </Form.Group>
+        
+        <Editor
+            editorState={editorState}
+            wrapperClassName="demo-wrapper"
+            editorClassName="demo-editor"
+            onEditorStateChange={onEditorStateChange}
+        />
+        <Button variant='primary' onClick={sendmail}>Send</Button>
+      </Form>
+    </div>
   )
 }
 
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,7 +3,7 @@ import { Nav, NavLink, Navbar, NavbarBrand } from 'react-bootstrap'
 
 const Topbar = () => {
   return (
-    <Navbar className='bg-white'>
+    <Navbar className='bg-white shadow-sm'>
       <NavbarBrand className='ms-3'>MyWebLink</NavbarBrand>
       <Nav>
         <NavLink>Home</NavLink>
